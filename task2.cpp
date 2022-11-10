b256d251e73f8340c2b6017a012227107a164e64
AikoCute <aikocute@icloud.com>
2022-11-10 08:18:31 +0700

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -5,22 +5,34 @@
     //Create 4 default phones.
     struct Phone {
         char Code [20];
-        char Name [50];
+        char Name [100];
         float Price;
         char ManuFacturer[20];
     };
 
     // Add new phones.
-    void AddPhone(struct Phone *p, int *n) {
-        printf("Enter the code of the phone: ");
-        scanf("%s", p[*n].Code);
-        printf("Enter the name of the phone: ");
-        scanf("%s", p[*n].Name);
-        printf("Enter the price of the phone: ");
-        scanf("%f", &p[*n].Price);
-        printf("Enter the manufacturer of the phone: ");
-        scanf("%s", p[*n].ManuFacturer);
-        *n = *n + 1;
+    struct Phone *addPhone(struct Phone *phone, int *size) {
+        int newSize = *size + 1;
+        struct Phone *newPhone = (struct Phone *) realloc(phone, newSize * sizeof(struct Phone));
+        if (newPhone == NULL) {
+            printf("Error! memory not allocated.");
+            exit(0);
+        }
+        printf("Enter Code: ");
+        scanf("%s", newPhone[newSize - 1].Code);
+
+        // Enter Name Phone : Iphone 13 Pro Max
+        printf("Enter Name: ");
+        scanf("%s", newPhone[newSize - 1].Name);
+
+        printf("Enter Price: ");
+        scanf("%f", &newPhone[newSize - 1].Price);
+
+        printf("Enter ManuFacturer: ");
+        scanf("%s", newPhone[newSize - 1].ManuFacturer);
+
+        *size = newSize;
+        return newPhone;
     }
 
     // Display all phones.
@@ -86,63 +98,65 @@
         fclose(f);
     }
 
+// main func
     int main() {
-        struct Phone p[100];
         int n = 4;
+        struct Phone *phone = (struct Phone *) malloc(n * sizeof(struct Phone));
+        strcpy(phone[0].Code, "P01");
+        strcpy(phone[0].Name, "Iphone");
+        phone[0].Price = 1000;
+        strcpy(phone[0].ManuFacturer, "Apple");
+        strcpy(phone[1].Code, "P02");
+        strcpy(phone[1].Name, "Samsung");
+        phone[1].Price = 900;
+        strcpy(phone[1].ManuFacturer, "Samsung");
+        strcpy(phone[2].Code, "P03");
+        strcpy(phone[2].Name, "Oppo");
+        phone[2].Price = 800;
+        strcpy(phone[2].ManuFacturer, "Oppo");
+        strcpy(phone[3].Code, "P04");
+        strcpy(phone[3].Name, "Vivo");
+        phone[3].Price = 700;
+        strcpy(phone[3].ManuFacturer, "Vivo");
         int choice;
-        strcpy(p[0].Code, "P01");
-        strcpy(p[0].Name, "Iphone");
-        p[0].Price = 1000.5;
-        strcpy(p[0].ManuFacturer, "Apple");
-        strcpy(p[1].Code, "P02");
-        strcpy(p[1].Name, "Samsung");
-        p[1].Price = 900.5;
-        strcpy(p[1].ManuFacturer, "Samsung");
-        strcpy(p[2].Code, "P03");
-        strcpy(p[2].Name, "Nokia");
-        p[2].Price = 800.0;
-        strcpy(p[2].ManuFacturer, "Nokia");
-        strcpy(p[3].Code, "P04");
-        strcpy(p[3].Name, "Oppo");
-        p[3].Price = 700.5;
-        strcpy(p[3].ManuFacturer, "Oppo");
         do {
-            printf("1. Add a new phone \n");
+            printf("1. Add new phone \n");
             printf("2. Display all phones \n");
             printf("3. Search for a phone by name \n");
             printf("4. Display sorted phones by name \n");
             printf("5. Save all phones to file ?dataP.txt? \n");
             printf("6. Load all phones from file ?dataP.txt? \n");
-            printf("0. Exit \n");
+            printf("7. Exit \n");
             printf("Enter your choice: ");
             scanf("%d", &choice);
             switch (choice) {
                 case 1:
-                    AddPhone(p, &n);
+                    phone = addPhone(phone, &n);
                     break;
                 case 2:
-                    DisplayPhone(p, n);
+                    DisplayPhone(phone, n);
                     break;
                 case 3:
-                    SearchPhone(p, n);
+                    SearchPhone(phone, n);
                     break;
                 case 4:
-                    SortPhone(p, n);
+                    SortPhone(phone, n);
                     break;
                 case 5:
-                    SavePhone(p, n);
+                    SavePhone(phone, n);
                     break;
                 case 6:
-                    LoadPhone(p, &n);
+                    LoadPhone(phone, &n);
                     break;
                 case 7:
                     printf("Goodbye! \n");
                     break;
                 default:
                     printf("No choice! \n");
+                    break;
             }
         } while (choice != 7);
         return 0;
     }
 
-
+    
